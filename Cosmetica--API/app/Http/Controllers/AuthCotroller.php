c58296ae02eadf4aa0a2f3ea196cf998f0fd52ba
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-16 13:30:18 +0000

    Enhance AuthController by adding necessary imports for authentication and request handling

--- Cosmetica--API/app/Http/Controllers/AuthCotroller.php
+++ Cosmetica--API/app/Http/Controllers/AuthCotroller.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisteRequest;
 
 class AuthCotroller extends Controller
 {
     //
+
+
 }

5177e1a77e79334e75cfb3ac517a7b1ac5954846
AbdellahLemtiri <lemtiriabdellah@gmail.com>
2026-03-31 11:35:44 +0100

    Add admin routes and update home page content in App component

--- cosmetica-front/src/App.jsx
+++ cosmetica-front/src/App.jsx
@@ -2,18 +2,33 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 
-const Home = () => <div className="p-10 text-3xl font-bold text-gray-800 text-center">Page d'accueil en construction 🚧</div>;
+ import AdminRoute from './components/AdminRoute';
+import AdminLayout from './components/AdminLayout';
+import Dashboard from './pages/admin/Dashboard';
+import Categories from './pages/admin/Categories';
+import Produits from './pages/admin/Produits';
 
+const Home = () => <div className="p-10 text-3xl font-bold text-center">Page d'accueil Client 🧴</div>;
+  
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 font-sans">
+    <div className="min-h-screen font-sans">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+         <Route element={<AdminRoute />}>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<Dashboard />} />  
+            <Route path="produits" element={<Produits />} />  
+            <Route path="categories" element={<Categories />} />  
+            <Route path="commandes" element={<div>Commandes</div>} />
+          </Route>
+        </Route>
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
